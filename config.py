810e848199b2d440bab0bce49d34f4c93fed218e
nhnminh1409 <nhnminh1409@gmail.com>
2026-05-03 16:28:40 +0700

    Connecting to DB

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ DB_HOST = "127.0.0.1"
 DB_USER = "root"
 DB_PASSWORD = "Ngminh@1409"
 DB_PORT = 3307
-DB_NAME = "hospital_management_db" # Update this to your actual database name
+DB_NAME = "hospital_db" # Update this to your actual database name
 
 def get_connection():
     """
